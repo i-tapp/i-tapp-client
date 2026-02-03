867e891eca5b447a060de8fbca22a4e02abcdf5c
sonkiee <annagu.kennedy@gmail.com>
2026-02-03 16:43:15 +0100

    vercel restart
    Signed-off-by: sonkiee <annagu.kennedy@gmail.com>

--- src/components/layouts/protected/header.tsx
+++ src/components/layouts/protected/header.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 import { Notification } from "iconsax-reactjs";
-
 import {
   Tooltip,
   TooltipContent,
