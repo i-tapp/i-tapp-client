8ac2929464ae6cc59f2086ec6b7a757202ed3b4c
sonkiee <annagu.kennedy@gmail.com>
2026-04-03 22:49:39 +0100

    feat: update robots and redirects for ads.txt access

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      allow: "/",
+      allow: ["/", "/ads.txt"],
     },
     sitemap: `${siteUrl}/sitemap.xml`,
   };
